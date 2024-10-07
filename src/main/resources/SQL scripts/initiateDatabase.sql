c1d2f3397a16117660c851a72df6fa6e19fad1f2
Daniel <danielpazirai@gmail.com>
2024-10-07 14:44:24 +0200

    altered user_table to include role

--- src/main/resources/SQL scripts/initiateDatabase.sql
+++ src/main/resources/SQL scripts/initiateDatabase.sql
@@ -6,6 +6,7 @@ CREATE TABLE Users (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100),
+    role VARCHAR(20) NOT NULL DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
